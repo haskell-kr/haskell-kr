4ea1fbb04188ff9bb8b60cb053c1216905c3fec6
XT <e@xtendo.org>
2018-05-22 17:29:53 +0900

    디플로이 코드 개선
    유닉스 사용자 이름을 파라미터화

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
 set -e
 CONFDIR=$(dirname $0)
+USER_PLUS_HOST="$1@$2"
+
+# Prepare local files
 $CONFDIR/../compile.sh
-scp $CONFDIR/haskell.kr.nginx.conf $1:/tmp/
-scp $CONFDIR/haskell.kr.service $1:/tmp/
-ssh $1 -C "
-mkdir -p /home/ubuntu/haskell.kr &&
-sudo mkdir -p /var/log/nginx/haskell.kr &&
-sudo cp /tmp/haskell.kr.nginx.conf /etc/nginx/conf.d/ &&
-sudo cp /tmp/haskell.kr.service /etc/systemd/system/ &&
+cat $CONFDIR/haskell.kr.nginx.conf | sed -e "s/{{USER}}/$1/" > /tmp/haskell.kr.nginx.conf
+cat $CONFDIR/haskell.kr.service | sed -e "s/{{USER}}/$1/" > /tmp/haskell.kr.service
+
+# Make destination directory
+ssh $USER_PLUS_HOST -C "
+mkdir -p ~/haskell.kr &&
+sudo mkdir -p /var/log/nginx/haskell.kr
+"
+
+# Copy
+scp /tmp/haskell.kr.nginx.conf $USER_PLUS_HOST:/tmp/
+scp /tmp/haskell.kr.service $USER_PLUS_HOST:/tmp/
+rsync -avzr $CONFDIR/../output $USER_PLUS_HOST:haskell.kr/
+rsync -avzr $CONFDIR/../static $USER_PLUS_HOST:haskell.kr/
+
+# Run remote commands
+ssh $USER_PLUS_HOST -C "
+sudo mv /tmp/haskell.kr.nginx.conf /etc/nginx/conf.d/ &&
+sudo mv /tmp/haskell.kr.service /etc/systemd/system/ &&
 sudo systemctl enable haskell.kr.service &&
 sudo systemctl start haskell.kr.service &&
 sudo systemctl reload nginx
 "
-rsync -avzr $CONFDIR/../output $1:haskell.kr/
-rsync -avzr $CONFDIR/../static $1:haskell.kr/
